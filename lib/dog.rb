82deabd4fcc00642ea5eb107a76ef401fcca9163
shmuly11 <shmuly11@gmail.com>
2020-12-14 22:24:47 -0500

    commit tree 81deb4532bb30aae3fd1f032c2519572ca1ed014

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
-# Add your code here
+require 'pry'
+class Dog
+    @@all = []
+    attr_reader :name
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+       # binding.pry
+      puts self.all.map(&:name)
+    end
+
+    def self.clear_all
+        self.all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+end
+
+#binding.pry
+0
